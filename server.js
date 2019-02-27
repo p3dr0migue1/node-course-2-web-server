e80856a3c8d8eec068a39c01ce35819c1748114d
Pedro Miguel <pedro.miguel@live.co.uk>
2019-02-27 22:22:59 +0000

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -17,9 +19,9 @@ app.use((req, res, next) => {
   });
   next();
 });
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 // `__dirname` stores the path to `node-web-server`
 // in other words `__dirname` stores the project path
 app.use(express.static(__dirname + '/public'));
@@ -48,6 +50,6 @@ app.get('/bad', (req, res) => {
     errorMessage: 'Error handling request'
   })
 })
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
